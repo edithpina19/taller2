37c7ff2bb56e834c592a22e2aaa6ad99eb3d7c9a
edithpina19 <pinaportilloedith2@gmail.com>
2025-12-11 17:07:07 -0600

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,8 +7,4 @@ urlpatterns = [
     path("bienvenido/", bienvenido_view, name="bienvenido"),
     path("mi-cuenta/", ver_mi_cuenta, name="mi_cuenta"),
     path("logout/", logout_view, name="logout"),
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html"), name='password_reset'),
-path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"), name='password_reset_done'),
-path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name='password_reset_confirm'),
-path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete.html"), name='password_reset_complete'),
 ]
