29a413238b13b456c5b1be337993c48b7c5dc948
edithpina19 <pinaportilloedith2@gmail.com>
2025-12-11 00:37:30 -0600

    Update settings.py

--- reparaciones/settings.py
+++ reparaciones/settings.py
@@ -126,10 +126,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # <--- ¡AGREGA ESTA LÍNEA!
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-# Aquí usamos las variables de entorno para NO poner tu clave real
-EMAIL_HOST_USER = os.getenv('EMAIL_USER') 
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
+
